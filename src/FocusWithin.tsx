e97c7460f4a7dbf33dfa7a6c9eb2c6707dcc8518
pan <robertpanvip@163.com>
2022-01-18 21:19:51 +0800

    [修改FocusWithin 在stopPropagation也能使用]

--- src/FocusWithin.tsx
+++ src/FocusWithin.tsx
@@ -10,6 +10,21 @@ interface FocusWithinProps {
     disabled?: boolean
 }
 
+const noop: () => void = () => undefined;
+
+const {stopPropagation} = FocusEvent.prototype
+
+FocusEvent.prototype.stopPropagation = function (...rest) {
+    this.path.forEach((node: any) => {
+        if (this.type === 'blur') {
+            node.__onBlur && node.__onBlur();
+        } else if (this.type === 'focus') {
+            node.__onFocus && node.__onFocus();
+        }
+    })
+    return stopPropagation.call(this, ...rest)
+}
+
 /***
  * 该组件的功能是类似于css伪类:focus-within
  * 即当前组件包裹的children的子组件获得焦点、那么当前组件包裹的children就会添加is-focus类
@@ -26,33 +41,52 @@ export default class FocusWithin extends React.PureComponent<FocusWithinProps> {
      */
     componentDidMount(): void {
         const keys = Object.keys(this.props)
-            .filter(key => key !== 'children' && key !== 'disabled'&& key !== 'focusClassName')
+            .filter(key => key !== 'children' && key !== 'disabled' && key !== 'focusClassName')
             .map(key => key + '属性无效，');
         warning(
             keys.length === 0,
             'FocusWithin',
             keys + 'FocusWithin组件只支持children、disabled、focusClassName属性'
         );
+
+        if (this.el.current) {
+            const __onFocus = this.el.current.__onFocus || noop;
+            this.el.current.__onFocus = () => {
+                this.handleFocus();
+                return __onFocus()
+            }
+            const __onBlur = this.el.current.__onBlur || noop;
+            this.el.current.__onBlur = () => {
+                this.handleBlur();
+                return __onBlur()
+            }
+        }
+
     }
 
     /**
      * 处理聚焦
      */
     private handleFocus(): void {
-        const {disabled,focusClassName} = this.props;
+        const {disabled, focusClassName} = this.props;
         if (!disabled) {
-            classList(this.el.current as HTMLElement).toggle(focusClassName)
+            const classes = classList(this.el.current as HTMLElement)
+            if (!classes.contains(focusClassName)) {
+                classes.add(focusClassName)
+            }
         }
-
     }
 
     /**
      * 处理失去焦点
      */
     private handleBlur() {
-        const {disabled,focusClassName} = this.props;
+        const {disabled, focusClassName} = this.props;
         if (!disabled) {
-            classList(this.el.current as HTMLElement).toggle(focusClassName)
+            const classes = classList(this.el.current as HTMLElement)
+            if (classes.contains(focusClassName)) {
+                classes.remove(focusClassName)
+            }
         }
     }
 
@@ -61,12 +95,6 @@ export default class FocusWithin extends React.PureComponent<FocusWithinProps> {
      */
     render(): React.ReactNode {
         const {children} = this.props;
-
-        const count = React.Children.count(children);
-        /*if (count !== 1) {
-            warning(false, 'FocusWidthIn', '不应该传入一个数组');
-            return children
-        }*/
         return (
             <Handler
                 ref={this.el}
