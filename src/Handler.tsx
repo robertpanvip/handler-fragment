6f8dcb97214598a1ee81d8367b328a0662b749ab
pan <robertpanvip@163.com>
2022-01-18 23:45:51 +0800

    [修改handle为函数式组件]

--- src/Handler.tsx
+++ src/Handler.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
-import autoBind from "./utils/autoBind";
-import omit from "./utils/omit";
-import {forwardRef} from "./utils/ref";
-import {combineRef, UUID,RefObject} from "./utils/util";
+import {useRef, useEffect, forwardRef} from 'react';
 import warning from "./utils/warning";
-import requestAnimationFrame from 'raf';
+import omit from "./utils/omit";
+import {UUID} from "./utils/util";
 
 interface HandlerProps extends React.DOMAttributes<HTMLElement> {
     children: React.ReactElement,
@@ -39,56 +37,118 @@ interface ParentNode extends Node {
     removeChildCallbacks?: Array<(...params: any[]) => CallbackReturn>
 }
 
-/***
- * 该组件的功能相当于在children之前用一个div去包裹了 事件全部由div捕获处理了 但是最终div是没有显示在页面上的
+interface Ins {
+    readonly key?: string;
+    parentNode?: ParentNode;
+    removedSibling?: ChildNode;
+    childNodes?: ChildNode[],
+    insertBefore: (callee: Node, newChild: Node, refChild: ParentNode) => CallbackReturn
+    replaceChild: (callee: Node, newChild: ParentNode, oldChild: Node) => CallbackReturn,
+    removeChild: (callee: Node, oldChild: ParentNode) => CallbackReturn,
+    assignmentRef: (node: Node) => void
+}
+
+
+/**
+ *
+ * @constructor
  */
-@forwardRef()
-@autoBind
-export default class Handler extends React.PureComponent<HandlerProps> {
-    private readonly directlyRef: RefObject<Node>;
+function HandlerFragment(props: HandlerProps, ref: any): React.ReactElement {
+    const el = useRef<HTMLDivElement>(null)
+    const ins = useRef<Ins>({
+        key: UUID(),
 
-    private el: React.RefObject<HTMLDivElement> = React.createRef<HTMLDivElement>();
-    private readonly key: string;
-    private parentNode: ParentNode;
-    private removedSibling: ChildNode;
-    private childNodes: ChildNode[];
+        /**
+         *给ref手动赋值
+         * @param node
+         */
+        assignmentRef: (node: Node): void => {
+            if (ref instanceof Function) {
+                ref(node)
+            } else {
+                ref.current = node;
+            }
+        },
 
-    /**
-     * 构造函数
-     * @param props
-     */
-    constructor(props: HandlerProps) {
-        super(props);
-        this.directlyRef = combineRef(props, true);
-        this.key = UUID();
-    }
+        /**
+         *在..之前插入
+         * @param callee
+         * @param newChild
+         * @param refChild
+         */
+        insertBefore: (callee: Node, newChild: Node, refChild: ParentNode): CallbackReturn => {
+            if (refChild.UUID === ins.current.key) {
+                return {
+                    valid: true,
+                    return: callee.insertBefore(newChild, ins.current.childNodes[0])
+                }
+            }
+        },
+        /**
+         *替换
+         * @param callee
+         * @param newChild
+         * @param oldChild
+         */
+        replaceChild: (callee: Node, newChild: ParentNode, oldChild: Node): CallbackReturn => {
+            if (newChild.UUID === ins.current.key) {
 
-    /**
-     *给ref手动赋值
-     * @param node
-     */
-    assignmentRef(node: Node): void {
-        if (this.directlyRef instanceof Function) {
-            this.directlyRef(node)
-        } else {
-            this.directlyRef.current = node;
+                ins.current.childNodes.forEach(childNode => {
+                    callee.replaceChild(newChild, childNode)
+                });
+
+                return {
+                    valid: true,
+                    return: oldChild
+                }
+            }
+        },
+        /**
+         *移除
+         * @param callee
+         * @param oldChild
+         */
+        removeChild: (callee: Node, oldChild: ParentNode): CallbackReturn => {
+            if (oldChild.UUID === ins.current.key) {
+                ins.current.childNodes.forEach(childNode => {
+                    callee.removeChild(childNode)
+                });
+                return {
+                    valid: true,
+                    return: oldChild
+                }
+            }
         }
+    })
+    const {children, disabled} = props;
+    const count = React.Children.count(children);
+    if (count !== 1 && children) {
+        warning(false, 'Handler', 'Since this component passes more than one child, the ref it gets can only get the first one');
+    }
+    let omitProps = omit(
+        props,
+        [
+            'onBeforeReplace',
+            'onAfterReplace',
+            'disabled',
+            'children',
+            'forwardRef'
+        ]);
+    if (disabled) {
+        omitProps = {}
     }
 
-    /**
-     * 组件挂载完成
-     */
-    componentDidMount(): void {
+    useEffect(() => {
         const {
             disabled,
             onBeforeReplace,
             onAfterReplace,
-        } = this.props;
-        const {parentNode, childNodes} = this.el.current;
-        this.parentNode = parentNode;
-        this.childNodes = Array.from(childNodes);
+        } = props;
+        const {parentNode, childNodes} = el.current;
+        ins.current.parentNode = parentNode;
+        ins.current.childNodes = Array.from(childNodes);
         if (!disabled) {
-            this.removedSibling = this.el.current.nextSibling;
+            ins.current.removedSibling = el.current.nextSibling;
             let shouldFocus = false;
             let activeElement: HTMLElement;
             const beforeReplace = (el: Node) => {
@@ -107,28 +167,29 @@ export default class Handler extends React.PureComponent<HandlerProps> {
                 onAfterReplace && onAfterReplace();
             };
 
-            (this.el.current as ParentNode).UUID = this.key;
+            (el.current as ParentNode).UUID = ins.current.key;
 
             /***
-             * 因为下面this.el.current的children被插入到this.el.current的parentNode中去了
-             * react库在fiber中保留的是this.el.current 操作的是this.el.current  而这个时候this.el.current 下面没有childNode
-             * 所以需要将this.el.current 的方法覆写一遍
+             * 因为下面el.current的children被插入到el.current的parentNode中去了
+             * react库在fiber中保留的是el.current 操作的是el.current  而这个时候el.current 下面没有childNode
+             * 所以需要将el.current 的方法覆写一遍
              * @param oldChild
              */
-            this.el.current.removeChild = (oldChild) => {
+            el.current.removeChild = (oldChild) => {
                 try {
-                    this.childNodes = this.childNodes.filter(node => node !== (oldChild as Node));
-                    if (this.childNodes.length === 0) {
+                    ins.current.childNodes = ins.current.childNodes.filter(node => node !== (oldChild as Node));
+                    if (ins.current.childNodes.length === 0) {
                         /**childNodes 之前只有一个 现在移除后就没有了 那么removedSibling=oldChild.nextSibling**/
-                        this.removedSibling = oldChild.nextSibling;
+                        ins.current.removedSibling = oldChild.nextSibling;
                     }
-                    const {length} = this.childNodes;
+                    const {length} = ins.current.childNodes;
                     /**删除操作交由父类去操作**/
                     const res = parentNode.removeChild(oldChild);
                     requestAnimationFrame(() => {
                         /**一般执行了removeChild 后可能会执行appendChild 这里判断如果没有执行appendChild 后应该给ref赋值**/
-                        if (this.childNodes.length === length) {
-                            this.assignmentRef(this.childNodes[0]);
+                        if (ins.current.childNodes.length === length) {
+
+                            ins.current.assignmentRef(ins.current.childNodes[0]);
                         }
                     });
                     return res;
@@ -137,18 +198,18 @@ export default class Handler extends React.PureComponent<HandlerProps> {
                     return null;
                 }
             };
-            this.el.current.replaceChild = (newChild, oldChild) => {
+            el.current.replaceChild = (newChild, oldChild) => {
                 try {
                     const res = parentNode.replaceChild(newChild, oldChild);
 
-                    this.childNodes = this.childNodes.map(node => {
+                    ins.current.childNodes = ins.current.childNodes.map(node => {
                         if (node === (oldChild as Node)) {
                             return newChild as ChildNode
                         }
                         return node;
                     });
 
-                    this.assignmentRef(this.childNodes[0]);
+                    ins.current.assignmentRef(ins.current.childNodes[0]);
                     return res;
                 } catch (e) {
                     warning(false, 'Handler', e.message)
@@ -157,9 +218,9 @@ export default class Handler extends React.PureComponent<HandlerProps> {
 
             };
 
-            this.el.current.insertBefore = (newChild, refChild) => {
-                const index = this.childNodes.findIndex(node => node === refChild);
-                this.childNodes.splice(index, 0, (newChild as unknown as ChildNode));
+            el.current.insertBefore = (newChild, refChild) => {
+                const index = ins.current.childNodes.findIndex(node => node === refChild);
+                ins.current.childNodes.splice(index, 0, (newChild as unknown as ChildNode));
                 let res;
                 try {
                     res = parentNode.insertBefore(newChild, refChild);
@@ -167,18 +228,18 @@ export default class Handler extends React.PureComponent<HandlerProps> {
                     warning(false, 'Handler', e.message)
                     res = null;
                 }
-                this.assignmentRef(this.childNodes[0]);
+                ins.current.assignmentRef(ins.current.childNodes[0]);
                 return res;
             };
 
-            this.el.current.appendChild = (newChild) => {
+            el.current.appendChild = (newChild) => {
                 try {
-                    const removedSibling = this.childNodes.length === 0 ? this.removedSibling : this.childNodes[this.childNodes.length - 1].nextSibling;
+                    const removedSibling = ins.current.childNodes.length === 0 ? ins.current.removedSibling : ins.current.childNodes[ins.current.childNodes.length - 1].nextSibling;
                     const res = !removedSibling
                         ? parentNode.appendChild(newChild)
                         : parentNode.insertBefore(newChild, removedSibling);
-                    this.childNodes.push(newChild as unknown as ChildNode);
-                    this.assignmentRef(this.childNodes[0]);
+                    ins.current.childNodes.push(newChild as unknown as ChildNode);
+                    ins.current.assignmentRef(ins.current.childNodes[0]);
                     return res
                 } catch (e) {
                     warning(false, 'Handler', e.message)
@@ -188,17 +249,17 @@ export default class Handler extends React.PureComponent<HandlerProps> {
 
             Array.from(childNodes).forEach(node => {
                 beforeReplace(node);
-                parentNode.insertBefore(node, this.el.current);
+                parentNode.insertBefore(node, el.current);
             });
 
             afterReplace();
 
-            parentNode.removeChild(this.el.current);
+            parentNode.removeChild(el.current);
 
             CallbacksMap.forEach(([methodsCallbacks, method]: [MethodsCallback, Methods]) => {
-                if (!this.parentNode[methodsCallbacks]) {
-                    this.parentNode[methodsCallbacks] = [];
-                    this.parentNode[method] = function (...params: any[]) {
+                if (!ins.current.parentNode[methodsCallbacks]) {
+                    ins.current.parentNode[methodsCallbacks] = [];
+                    ins.current.parentNode[method] = function (...params: any[]) {
                         let res;
                         for (let i = 0; i < this[methodsCallbacks].length; i++) {
                             const callback = this[methodsCallbacks][i];
@@ -226,125 +287,45 @@ export default class Handler extends React.PureComponent<HandlerProps> {
                 }
             });
 
-            this.parentNode.insertBeforeCallbacks.push(this.insertBefore)
-            this.parentNode.removeChildCallbacks.push(this.removeChild)
-            this.parentNode.removeChildCallbacks.push(this.replaceChild)
+            ins.current.parentNode.insertBeforeCallbacks.push(ins.current.insertBefore)
+            ins.current.parentNode.removeChildCallbacks.push(ins.current.removeChild)
+            ins.current.parentNode.removeChildCallbacks.push(ins.current.replaceChild)
 
-            this.assignmentRef(this.childNodes[0]);
+            ins.current.assignmentRef(ins.current.childNodes[0]);
 
         }
-    }
 
-    /**
-     *在..之前插入
-     * @param callee
-     * @param newChild
-     * @param refChild
-     */
-    insertBefore(callee: Node, newChild: Node, refChild: ParentNode): CallbackReturn {
-        if (refChild.UUID === this.key) {
-            return {
-                valid: true,
-                return: callee.insertBefore(newChild, this.childNodes[0])
+        return () => {
+            const {children} = props;
+            const count = React.Children.count(children);
+            if (count !== 1 && children) {
+                return;
             }
-        }
-    }
-
-    /**
-     *替换
-     * @param callee
-     * @param newChild
-     * @param oldChild
-     */
-    replaceChild(callee: Node, newChild: ParentNode, oldChild: Node): CallbackReturn {
-        if (newChild.UUID === this.key) {
-
-            this.childNodes.forEach(childNode => {
-                callee.replaceChild(newChild, childNode)
-            });
-
-            return {
-                valid: true,
-                return: oldChild
-            }
-        }
-    }
-
-    /**
-     *移除
-     * @param callee
-     * @param oldChild
-     */
-    removeChild(callee: Node, oldChild: ParentNode): CallbackReturn {
-        if (oldChild.UUID === this.key) {
-            this.childNodes.forEach(childNode => {
-                callee.removeChild(childNode)
-            });
-            return {
-                valid: true,
-                return: oldChild
-            }
-        }
-    }
-
-    /**
-     * 组件销毁
-     */
-    componentWillUnmount(): void {
-        const {children} = this.props;
-        const count = React.Children.count(children);
-        if (count !== 1 && children) {
-            return;
-        }
-        requestAnimationFrame(() => {
-            CallbacksMap.forEach(([methodsCallbacks, method]: [MethodsCallback, Methods]) => {
-                this.parentNode[methodsCallbacks] = this.parentNode[methodsCallbacks].filter(callback => callback !== (this as any)[method]);
+            requestAnimationFrame(() => {
+                CallbacksMap.forEach(([methodsCallbacks, method]: [MethodsCallback, Methods]) => {
+                    ins.current.parentNode[methodsCallbacks] = ins.current.parentNode[methodsCallbacks].filter(callback => callback !== ins.current[method]);
+                })
             })
-        })
-    }
-
-
-    /**
-     * 渲染函数
-     */
-    render(): React.ReactNode {
-        const {children, disabled} = this.props;
-        const count = React.Children.count(children);
-        if (count !== 1 && children) {
-            warning(false, 'Handler', 'Since this component passes more than one child, the ref it gets can only get the first one');
         }
-        let omitProps = omit(
-            this.props,
-            [
-                'onBeforeReplace',
-                'onAfterReplace',
-                'disabled',
-                'children',
-                'forwardRef'
-            ]);
-        if (disabled) {
-            omitProps = {}
-        }
-        return (
-            <div
-                ref={this.el}
-                {...omitProps}
-                style={{
-                    display: 'inline-block',
-                    padding: 0,
-                    margin: 0,
-                    outline: 'none',
-                    border: 'none',
-                    width: '100%',
-                    height: '100%'
-                }}
-            >
-                {children}
-            </div>
+    }, [])
 
-        )
-
-
-    }
+    return (
+        <div
+            ref={el}
+            {...omitProps}
+            style={{
+                display: 'inline-block',
+                padding: 0,
+                margin: 0,
+                outline: 'none',
+                border: 'none',
+                width: '100%',
+                height: '100%'
+            }}
+        >
+            {children}
+        </div>
+    )
 }
 
+export default forwardRef(HandlerFragment)
